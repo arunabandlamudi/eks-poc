033951ac6a00d755c665a6914decdf2c39c4d288
arunabandlamudi <149061755+arunabandlamudi@users.noreply.github.com>
2023-11-23 17:30:44 +0530

    Update 4-subnet.tf
    updating subnets

--- 4-subnet.tf
+++ 4-subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -12,7 +12,7 @@ resource "aws_subnet" "public-1" {
 resource "aws_subnet" "public-2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 
   tags = {
@@ -23,7 +23,7 @@ resource "aws_subnet" "public-2" {
 resource "aws_subnet" "private-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.10.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-south-1a"
 
 
   tags = {
@@ -34,7 +34,7 @@ resource "aws_subnet" "private-1" {
 resource "aws_subnet" "private-2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.11.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-south-1b"
 
 
   tags = {
