729feb89316a60b119193c7dc5d5887a59282e29
arunabandlamudi <149061755+arunabandlamudi@users.noreply.github.com>
2023-11-26 21:05:35 +0530

    Update opensearch.tf
    updating

--- opensearch.tf
+++ opensearch.tf
@@ -1,18 +1,40 @@
-resource "aws_opensearch_domain" "poc" {
-  domain_name = "opensearch-poc"
+resource "aws_elasticsearch_domain" "default" {
+  domain_name           = var.domain_name
+  elasticsearch_version = var.elasticsearch_version
 
-  cluster_config {
-    instance_type  = "t3.small.search"
-    instance_count = 2
-  }
+  advanced_options = var.advanced_options
 
   ebs_options {
-    ebs_enabled = true
-    volume_size = 10
+    ebs_enabled = var.ebs_volume_size > 0 ? true : false
+    volume_size = var.ebs_volume_size
+    volume_type = var.ebs_volume_type
+    iops        = var.ebs_iops
+  }
+
+  encrypt_at_rest {
+    enabled    = var.encrypt_at_rest_enabled
+    kms_key_id = var.encrypt_at_rest_kms_key_id
+  }
+
+  cluster_config {
+    instance_count           = var.instance_count
+    instance_type            = var.instance_type
+    dedicated_master_enabled = var.dedicated_master_enabled
+    dedicated_master_count   = var.dedicated_master_count
+    dedicated_master_type    = var.dedicated_master_type
+    zone_awareness_enabled   = var.zone_awareness_enabled
+
+    zone_awareness_config {
+      availability_zone_count = var.availability_zone_count
+    }
+  }
+
+  node_to_node_encryption {
+    enabled = var.node_to_node_encryption_enabled
   }
 
   tags = {
-    Terraform   = "true"
-    Environment = "production"
+    Domain = "TestDomain"
   }
+
 }
